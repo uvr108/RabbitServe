12fb061472e7326e13d473ae78224217c3c3163d
uvergara <uvergara@csn.uchile.cl>
2020-01-29 15:47:16 -0300

    Se arreglaron fechas

--- receive_log_topic.js
+++ receive_log_topic.js
@@ -3,7 +3,7 @@ var amqp = require('amqplib/callback_api');
 var connect = null;
 
 r = require('rethinkdb');
-r.connect( {host: '127.0.0.1', db: 'csn', port: 28015}, function(err, conn) {
+r.connect( {host: '10.54.217.83', db: 'csn', port: 28015}, function(err, conn) {
     if (err) throw err;
     connect = conn;
 })
@@ -30,10 +30,10 @@ function ejecuta(mensaje) {
 function ingresar(mensaje, count) {
     mensaje['version'] = count;
     r.table('seiscomp').insert(mensaje).run(connect)
-    console.log(JSON.stringify(mensaje));
+    // console.log(JSON.stringify(mensaje));
 }
 
-amqp.connect('amqp://statistics:stdpass@thumper.lan.csn.uchile.cl:5672', function(error0, connection) {
+amqp.connect('amqp://statistics:stdpass@shogouki.lan.csn.uchile.cl:5672', function(error0, connection) {
     if (error0) {
         throw error0;
     }
@@ -57,45 +57,58 @@ amqp.connect('amqp://statistics:stdpass@thumper.lan.csn.uchile.cl:5672', functio
             channel.bindQueue(q.queue, exchange,"statistics.*");
 
             channel.consume(q.queue, function(msg) {
-                // console.log("%s", msg.content);
-                // var time = msg.content['time'];
+            
                 
                 let midict = JSON.parse(msg.content.toString());
                  if (midict['data']) {
                      midict = midict['data']
                  }
-                 
-
+                
                 // console.log(`xxx : ${JSON.stringify(midict)}`);
                 
                 insert = {};
 
                 let event_id = midict['event_id'];
-
+                
                 if (midict['from_mail']) {
-
-                        let from_mail =  midict['from_mail'];
-                        // console.log(`from_mail : ${JSON.stringify(from_mail)}`);
-                        time = from_mail['time'] = ((from_mail['time']) ? new Date(from_mail['time']): null);
                         
-                        year = time.getFullYear();
-                        month = time.getMonth()+1;
+                        
+                        from_mail = midict['from_mail'];
 
-                        mail_time = from_mail['mail_time'] = ((from_mail['mail_time']) ? new Date(from_mail['mail_time']): null);
+                        mail_time = from_mail['mail_time'];
+                        time = from_mail['time'];
 
-                        insert['email_delay'] = ((mail_time) ? Math.round((time - mail_time) / (60 * 60)) : null)
-                        insert['time'] = time;
+                        /*    FECHA_EMAIL    */
+                        
+                        let m = JSON.stringify(mail_time).substring(0,20).replace(/\"/g,'')+'+0000';
+                        let n = new Date(Date.parse(m));
+                        let fecha_email = n.toUTCString();
+                        let epoch_email = new Date(m.substring(0,4),m.substring(5,7), m.substring(8,10), m.substring(11,13), m.substring(14,16),  m.substring(17,19)).getTime()/1000;
+
+                        /*    FECHA_EVEN    */
+
+                        let s = JSON.stringify(time + '+0000').replace(' ','T').replace(/\//g,'-').replace(/\"/g,'');
+                        let d = new Date(Date.parse(s));
+                        let fecha_even = d.toUTCString();
+                        let epoch_event = new Date(s.substring(0,4),s.substring(5,7), s.substring(8,10), s.substring(11,13), s.substring(14,16),  s.substring(17,19)).getTime()/1000;
+      
+                        let year = Number(s.substring(0,4));
+                        let month = Number(s.substring(5,7));
+
+                        let diff = Number(((epoch_email - epoch_event)/60).toFixed(2));
+
+                        insert['fecha_even'] = fecha_even;
+                        insert['fecha_email'] = fecha_email;
                         insert['year'] = year;
                         insert['month'] = month;
                         insert['n5']   = (((from_mail['evaluation_status'] == 'preliminary') && (from_mail['delay'] > 5)) ? true : null);
                         insert['n20']   = (((from_mail['evaluation_status'] == 'final') && (from_mail['delay'] > 20)) ? true : null);
-                        insert['year'] = year;
-                        insert['month'] = month;
                         insert['event_id'] = event_id;
                         insert['lat'] = from_mail['lat'];
                         insert['lon'] = from_mail['lon'];
                         insert['mag'] = from_mail['mag'];
                         insert['process_delay'] = from_mail['delay'];
+                        insert['email_delay'] = diff;
                         insert['mag_type'] = from_mail['mag_type'];
                         insert['station_count'] = from_mail['station_count'];
                         insert['evaluation_status'] = evaluation =  from_mail['evaluation_status'];
@@ -103,15 +116,18 @@ amqp.connect('amqp://statistics:stdpass@thumper.lan.csn.uchile.cl:5672', functio
                         insert['author'] = ((from_mail['author']) ? from_mail['author']: null);
                         insert['sensible'] = null;
                         insert['version'] = null;
-                        console.log(`insert : ${insert}`);     
+                        // console.log(`TIME : ${event_id} ${from_mail['time']} ${time} : email_delat -> ${insert['email_delay']} []: ${t} - ${mail_time}`);
+                        // console.log(`[event_id] ${event_id}`);
+                        // console.log(`insert : ${JSON.stringify(insert)}`);     
                         ejecuta(insert);                       
 
                 }
                 if (midict['from_sensitive']) {
                     let from_sensitive =  midict['from_sensitive'];
                     r.db('csn').table('seiscomp').filter({'event_id': event_id}).update({'sensible':true}).run(connect)
-                    console.log(`from_sensitive yyy : ${event_id} ${JSON.stringify(from_sensitive)}`);
+                    // console.log(`from_sensitive yyy : ${event_id} ${JSON.stringify(from_sensitive)}`);
                 }
+            
                     
             }, {
                 noAck: false
